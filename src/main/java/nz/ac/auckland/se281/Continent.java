8ffd84684462c19de806914a7e702d70931ec0b6
Suah Kim <skim460@aucklanduni.ac.nz>
2025-05-26 13:10:52 +1200

    feat: add Continent enum with string mapping and lookup method

---
+++ src/main/java/nz/ac/auckland/se281/Continent.java
@@ -0,0 +1,29 @@
+package nz.ac.auckland.se281;
+
+public enum Continent {
+  ASIA("Asia"),
+  NORTH_AMERICA("North America"),
+  SOUTH_AMERICA("South America"),
+  AFRICA("Africa"),
+  AUSTRALIA("Australia"),
+  EUROPE("Europe");
+
+  private final String name;
+
+  Continent(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public static Continent fromString(String text) {
+    for (Continent continent : Continent.values()) {
+      if (continent.name.equalsIgnoreCase(text)) {
+        return continent;
+      }
+    }
+    return null;
+  }
+}
