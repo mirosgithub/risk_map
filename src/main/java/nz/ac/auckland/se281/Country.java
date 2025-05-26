64917e761a6c462f9c418e3779f6a3580d561d47
Suah Kim <skim460@aucklanduni.ac.nz>
2025-05-26 13:12:44 +1200

    feat: implement Country class with continent, fuel cost, and equality methods

---
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -0,0 +1,42 @@
+package nz.ac.auckland.se281;
+
+public class Country {
+
+  private String name;
+  private Continent continent;
+  private int fuelCost;
+
+  public Country(String name, String continent, String fuelCost) {
+    this.name = name;
+    this.continent = Continent.fromString(continent);
+    this.fuelCost = Integer.valueOf(fuelCost);
+  }
+
+  public Continent getContinent() {
+    return this.continent;
+  }
+
+  public int getFuelCost() {
+    return this.fuelCost;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Country other = (Country) obj;
+    if (name == null) {
+      if (other.name != null) return false;
+    } else if (!name.equals(other.name)) return false;
+    return true;
+  }
+}
