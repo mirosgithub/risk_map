9b34ae78dd2b9198bf5c87cb2769f50f57fa85cd
Suah Kim <skim460@aucklanduni.ac.nz>
2025-05-26 13:43:05 +1200

    feat: implement MapEngine to load country data and initialize graph structure

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,19 +1,39 @@
 package nz.ac.auckland.se281;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /** This class is the main entry point. */
 public class MapEngine {
 
+  private Graph graph = new Graph();
+  private Set<Country> countries = new HashSet<>();
+
   public MapEngine() {
-    // add other code here if you wan
-    loadMap(); // keep this mehtod invocation
+    // add other code here if you want
+    loadMap(); // keep this method invocation
   }
 
-  /** invoked one time only when constracting the MapEngine class. */
+  /** invoked one time only when constructing the MapEngine class. */
   private void loadMap() {
 
+    // e.g. Country,Continent,n
     List<String> countries = Utils.readCountries();
+
+    for (String countryData : countries) {
+      String[] parts = countryData.split(",");
+
+      String name = parts[0];
+      String continent = parts[1];
+      String fuelCost = parts[2];
+
+      Country country = new Country(name, continent, fuelCost);
+
+      this.countries.add(country);
+      this.graph.addNode(country);
+    }
+
     List<String> adjacencies = Utils.readAdjacencies();
   }
 
